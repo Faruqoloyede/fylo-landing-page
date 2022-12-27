7d47cf2c361288f7393c886c23152a2ec32927b2
Faruqoloyede <oloyedefaruq2@gmail.com>
2022-12-27 14:26:49 +0100

    added validation of email

--- app.js
+++ app.js
@@ -4,10 +4,32 @@ const observer = new IntersectionObserver((entries) => {
         if (entry.isIntersecting) {
             entry.target.classList.add('show');
         } else {
-            entry.target.classList.remove('show');
+            // entry.target.classList.remove('show');
         }
     });
 });
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+//validate email
+
+email.onkeydown = validate;
+
+function validate(){
+    const email = document.querySelector("#email").value;
+const form = document.querySelector("#form");
+const pattern = /^[^ ]+\.[a-z]{2,3}$/;
+const text = document.querySelector("#text");
+
+if(email.match(pattern)){
+    form.classList.add("valid");
+    form.classList.remove("invalid")
+    text.innerHTML = " "
+}else {
+    form.classList.add("invalid");
+    form.classList.remove("valide")
+    text.innerHTML = "please enter the correct email address!";
+    text.style.color = "#ff0000"
+}
+}
